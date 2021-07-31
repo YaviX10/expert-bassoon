1a76680d40041f62c6cd8043ddbcbef0dcf6f935
YaviX10 <88088722+YaviX10@users.noreply.github.com>
2021-07-31 15:19:13 +0530

    Add files via upload

---
+++ ayaanchutiyahe/lib/values/radii.dart
@@ -0,0 +1,14 @@
+/*
+*  radii.dart
+*  Draft_1
+*
+*  Created by Artem.
+*  Copyright © 2018 Supernova. All rights reserved.
+    */
+
+import 'package:flutter/rendering.dart';
+
+
+class Radii {
+  static const BorderRadiusGeometry k5pxRadius = BorderRadius.all(Radius.circular(5));
+}
